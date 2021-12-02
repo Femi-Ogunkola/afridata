a591fec2e74769ba8ed0ce16a3a85343688ab468
Obafemi Ogunkola <ogunkolaobafemi@outlook.com>
2021-12-02 23:28:34 +0100

    .

--- season/store/saver.py
+++ season/store/saver.py
@@ -12,40 +12,4 @@ class Saver:
         self.col = self.dbName.player
 
     def insert_one(self, playerJson: Dict):
-        self.col.insert_one(playerJson) 
-        
-
-
-
-
-#try:
-#	conn = MongoClient("mongodb+srv://obafemi:obafemi@cluster0.kawbt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-#	print("Connected successfully!!!")
-#except:
-#	print("Could not connect to MongoDB")
-
-# database
-#db = conn.afriskaut
-
-# Created or Switched to collection names: my_gfg_collection
-#collection = db.player
-'''emp_rec1 = {
-		"name":"Mr.Geek",
-		"eid":24,
-		"location":"delhi"
-		}
-emp_rec2 = {
-		"name":"Mr.Shaurya",
-		"eid":14,
-		"location":"delhi"
-		}
-# Insert Data
-rec_id1 = collection.insert_one(emp_rec1)
-rec_id2 = collection.insert_one(emp_rec2)
-
-print("Data inserted with record ids",rec_id1," ",rec_id2)
-
-# Printing the data inserted
-cursor = collection.find()
-for record in cursor:
-	print(record)'''
+        self.col.insert_one(playerJson)
